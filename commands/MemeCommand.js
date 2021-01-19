3495c37ef52d08a0aba37617247d6b084d7117e1
TZockt <info@tzockt.de>
2021-01-19 22:20:22 +0100

    Add Error Message by Nlmyr

--- commands/MemeCommand.js
+++ commands/MemeCommand.js
@@ -6,27 +6,36 @@ module.exports = {
     name: "meme",
     description: "This is a Meme Command!",
     async execute(message, args){
-        var sbr = "";
-        if(args.length == 1){
-            sbr = args[0]
+        try {
+            var sbr = "";
+            if(args.length == 1){
+                sbr = args[0]
+            }
+            const post = await redditimage.fetch({ type: "custom", total: 1, subreddit:[sbr] });
+            const data = post[0]
+
+            console.log(data)
+
+            //const msg = await message.channel.send("Loading...")
+
+            const Embed = new Discord.MessageEmbed()
+                .setTitle(`${data.title}`)
+                .setURL(data.postLink)
+                .setFooter(`Posted by ${data.author}`)
+                .setImage(data.image)
+                .setAuthor("Send from: " + message.author.username, `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`)
+
+            // await msg.delete()
+            // await message.delete()
+            message.channel.send(Embed)
+        } catch (error) {
+            const ErrEmbed = new Discord.MessageEmbed()
+                .setColor("DARK_RED")
+                .setTitle("Fehler!")
+                .setDescription(error)
+
+            message.channel.send(ErrEmbed)
         }
-        const post = await redditimage.fetch({ type: "custom", total: 1, subreddit:[sbr] });
-        const data = post[0]
-
-        console.log(data)
-
-        //const msg = await message.channel.send("Loading...")
-
-        const Embed = new Discord.MessageEmbed()
-        .setTitle(`${data.title}`)
-        .setURL(data.postLink)
-        .setFooter(`Posted by ${data.author}`)
-        .setImage(data.image)
-        .setAuthor("Send from: " + message.author.username, `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`)
-
-        // await msg.delete()
-        // await message.delete()
-        message.channel.send(Embed)
         
     }
 }
